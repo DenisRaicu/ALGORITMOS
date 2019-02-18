6440c97f98f6f3f3172d07d2f41b2535df6c6937
Denis <denisrai@ucm.es>
2019-02-18 22:20:13 +0100

    Add files via upload

---
+++ Recursión Simple y Multiple/18.cpp
@@ -0,0 +1,79 @@
+﻿// Denis Raicu
+// E49
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using lli = long long int;
+
+// función que resuelve el problema con recursión multiple:
+// El coste de esta función es exponencial: O (2^n) siendo n
+// el la posición sobre la cual se quiere calcular el numero de 
+// Fibonacci --->
+// T(n) = {
+//			c1							n == 0
+//			c1							n == 1
+//			T(n - 1) + T(n - 2) + c2	n > 1
+// }
+/*lli resolverMultiple(int num) {
+
+	if (num == 0) return 0;
+	else if (num == 1) return 1;
+	else return (resolverMultiple(num - 1) + resolverMultiple(num - 2));
+}*/
+
+// funcion que resuelve el problema con recursión simple:
+// El coste de esta función es lineal: O(n) siendo n el 
+// la posición sobre la cual se quiere calcular el numero de 
+// Fibonacci --->
+// T(n) = {
+//			c1							n == 0
+//			c1							n == 1
+//			T(n-1) + c2					n > 1
+// }
+lli resolverSimple(int num, lli a, lli b) {
+
+	if (num == 0) return b;
+	else if (num == 1) return a;
+
+	else return resolverSimple(num - 1, a + b, a);
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int num;
+	std::cin >> num;
+
+	if (!std::cin) return false;
+
+	// escribir sol (recursividad simple):
+	std::cout << resolverSimple(num, 1, 0) << "\n";
+
+	// escribir sol (recursividad multiple):
+	// std::cout << resolverMultiple(num) << "\n";
+
+	return true;
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	//save old buf and redirect std::cin to datos.txt:
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif 
+
+	while (resuelveCaso());
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ Recursión Simple y Multiple/19.cpp
@@ -0,0 +1,66 @@
+﻿// Denis Raicu
+// E49
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+// función que resuelve el problema
+int resolver(int a, int b, std::vector<std::vector<int>> & m) {
+	
+	// Comprobamos los dos casos base: (b == 0 || a == b) ==> 1; 
+	if (b == 0 || b == a) return m[a][b] = 1; 
+	else { // Si no estamos en ningun caso base, montamos la formula;
+		// Si la el numero combinatorio no esta calculado, seguimos con la recursion:
+		if (m[a][b] == -1 ) { 
+			// Busco la  primera parte de la formula (a-1, b-1):
+			if (m[a-1][b-1] == -1) m[a-1][b-1] = resolver(a-1, b-1, m); 
+			// Busco la segunda parte de la formula (a-1, b):
+			if (m[a-1][b] == -1) m[a-1][b] = resolver(a-1, b, m); 
+			
+			return m[a][b] = m[a-1][b-1] + m[a-1][b]; // Sumamos las dos partes;
+		} 
+
+		//En el caso de  que el numero combinatorio buscado ya se ha calculado de antes:
+		else return m[a][b];
+	}
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int a, b;
+	std::cin >> a;
+
+	if (!std::cin) return false;
+	std::cin >> b;
+
+	std::vector<std::vector<int>> m(a+1, std::vector<int>(b+1, -1));
+
+	// escribir sol
+	std::cout << resolver(a, b, m) << "\n";
+
+	return true;
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	//save old buf and redirect std::cin to datos.txt
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif 
+
+	while (resuelveCaso());
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
